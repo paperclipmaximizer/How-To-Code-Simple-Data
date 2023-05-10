17e1ea85950f55c30e3ebb1c662c2ae11784ba31
Liam <50953884+paperclipmaximizer@users.noreply.github.com>
2023-05-10 15:00:25 +1000

    solutions

--- final/space-invaders-starter.rkt
+++ final/space-invaders-starter.rkt
@@ -1,3 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname space-invaders-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/universe)
 (require 2htdp/image)
 
@@ -36,7 +39,7 @@
 
 (define MISSILE (ellipse 5 15 "solid" "red"))
 
-
+ 
 
 ;; Data Definitions:
 
